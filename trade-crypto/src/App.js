0c899f4ba675810284985695a9edb8a47ce8f39c
Dennis Orbison <orbison.dennis@gmail.com>
2018-12-14 15:26:00 -0800

    btc balance working correctly

--- trade-crypto/src/App.js
+++ trade-crypto/src/App.js
@@ -2,24 +2,48 @@ import React, { Component } from 'react';
 import Form from './Components/Form'
 import './App.css';
 
+// needed Inputs
+// btc price
+// doge price
+// usd capital
+// total trades or days to trade
+// trade cycle
+// term period || term rate
+// recurring capital
+// daily trade rate
+// randomize true or false // trade cycle // daily trade rate
+
+// minimumTradeRate: 50000,
+// tradeMultiplier: 2,
+// sellRate: 0.5,
+// buyRate: [],
+// feeRate: 0.002,
+
 class App extends Component {
 
   state = {
     dogePrice: '',
     btcPrice: null,
     dogeQuantity: null,
-    amount: null,  // doge
-    price: null,  // btc
-    sum: null,  // btc
+    amount: '', // doge
+    price: '',  // btc
+    sum: '',  // btc
     fee: null,  // btc
     total: null,  // btc
   }
 
   handleOnSubmit = ({dogePrice, dogeAmount, btcSum}) => {
+
     console.log(dogePrice, dogeAmount, btcSum)
 
+    console.log( dogePrice + '0'.repeat(8 - dogePrice.split('.')[1].length))
+
+    
+
     this.setState({
-      price: dogePrice,
+      price:  dogePrice.split('.').length > 1 ? dogePrice + '0'.repeat(8 - dogePrice.split('.')[1].length) + ' BTC' : dogePrice + '.00000000 BTC' ,
+      amount: dogeAmount.split('.').length > 1 ? dogeAmount + '0'.repeat(8 - dogeAmount.split('.')[1].length) + ' DOGE' : dogeAmount + '.00000000 DOGE' ,
+      sum: btcSum.split('.').length > 1 ? btcSum + '0'.repeat(8 - btcSum.split('.')[1].length)  + ' BTC' : btcSum + '.00000000 BTC' ,
     })
   }
 
@@ -33,6 +57,8 @@ class App extends Component {
         />
 
         <h1>{String(this.state.price)}</h1>
+        <h1>{String(this.state.amount)}</h1>
+        <h1>{String(this.state.sum)}</h1>
 
       </div>
     );
--- trade-crypto/src/Components/Form.js
+++ trade-crypto/src/Components/Form.js
@@ -1,75 +1,145 @@
 import React from 'react';
+import styled from 'styled-components'
+
+const FormContent = styled.div`
+  border: 1px solid red;
+  display: flex;
+  flex-direction: column;
+
+  form {
+    border: 1px solid green;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    font-size: 20px;
+  }
+
+  input {
+    text-align: right;
+    margin-bottom: 20px;
+  }
+`;
+
 
 class Form extends React.Component {
 
   state = {
+    btcBalance: '0.10000000',
+    btcNewBalance: '',
     dogePrice: '',
     dogeAmount: '0.00000000',
     btcSum: '0.00000000',
+    fee: '',
+    feeRate: 0.002,
+    total: '',
 
+    btcPrice: '',
+    usdAmount: '',
+    btcQuantity: ''
   }
 
   handleOnChange = e => {
 
+    function cryptoValue(num) {
+      return Math.round(Number(num) * Math.pow(10,8));
+    }
+
     let a,b,c;
+    let d = this.state.btcPrice;
+    a = cryptoValue(this.state.dogePrice);
+    b = cryptoValue(this.state.dogeAmount);
+    c = cryptoValue(this.state.btcSum); 
 
     if(e.target.name === 'dogePrice') {
-      a = Math.round(Number(e.target.value) * Math.pow(10,8)); 
-      b = Math.round(Number(this.state.dogeAmount) * Math.pow(10,8)); 
-      c = Math.round(Math.round(a * b) / Math.pow(10,8));
-      console.log(c)
+      a = cryptoValue(e.target.value); 
+      b = cryptoValue(this.state.dogeAmount); 
+      c = Math.round(Math.round(a * b) / Math.pow(10,8));  
+
     } else if(e.target.name === 'dogeAmount') {
-      a = Math.round(Number(this.state.dogePrice) * Math.pow(10,8));
-      b = Math.round(Number(e.target.value) * Math.pow(10,8));
+      a = cryptoValue(this.state.dogePrice);
+      b = cryptoValue(e.target.value);
       c = Math.round(Math.round(a * b) / Math.pow(10,8));
-      console.log(c / Math.pow(10,8), a * b, 'c')
+
     } else if(e.target.name === 'btcSum') {
-      a = Math.round(Number(this.state.dogePrice) * Math.pow(10,8));
-      c = Math.round(Number(e.target.value) * Math.pow(10,8));
+      a = cryptoValue(this.state.dogePrice);
+      c = cryptoValue(e.target.value);
       b = Math.round(1/a * c * Math.pow(10,8));
+
+    } else if(e.target.name === 'btcPrice'){
+     
+      d = e.target.value;
+    } else if(e.target.name === 'usdAmount') {
+
+    } else if(e.target.name === 'btcQuantity') {
+
     }
+
+    let total = Math.round(this.state.feeRate * c + c);
+
+    console.log( total, cryptoValue(this.state.btcBalance) - total);
+
+
+
+    //console.log(e.target.name === 'btcPrice' ? ` $${e.target.value}`: e.target.value,)
 
     this.setState({
       dogePrice: cryptoFormat(a),
       dogeAmount: cryptoFormat(b),
       btcSum: cryptoFormat(c),
-    })
-
-    console.log(Number(e.target.value) * Math.pow(10,8))
-
-    this.setState({
-      [e.target.name]: e.target.value
+      fee: cryptoFormat(this.state.feeRate * c),
+      total: cryptoFormat(total),
+      btcNewBalance: cryptoFormat(cryptoValue(this.state.btcBalance) - total),
+      
+      btcPrice: d,
+      [e.target.name]: e.target.value,
     })
 
     function cryptoFormat(num) {
         
 
       if(num < 0) {
+
         num = Math.abs(num)
         num = num.toString();
         return num.length <= 8 
           ? '-0.' + '0'.repeat(8 - num.length) + num
+          : `-${num.slice(0,-8)}.${num.slice(-8)}`;
+
+      } else {
+
+        num = num.toString();
+        return num.length <= 8 
+          ? '0.' + '0'.repeat(8 - num.length) + num 
           : `${num.slice(0,-8)}.${num.slice(-8)}`;
+
       }
 
-      num = num.toString();
-      return num.length <= 8 
-        ? '0.' + '0'.repeat(8 - num.length) + num 
-        : `${num.slice(0,-8)}.${num.slice(-8)}`;
-    }
-  }
+    } // cryptoFormat(num)
+
+  } // handleOnChange(e)
 
   render() {
 
     return (
-      <div>
+      <FormContent>
 
         <h1>is working in Form.js</h1>
 
-        <form onSubmit = { e => {
+        <form 
+          autoComplete="off"
+          onSubmit = { e => {
           e.preventDefault();
           this.props.handleOnSubmit(this.state);
         } }>
+
+           <label> BTC balance: </label>
+          <input
+            name = {'btcBalance'}
+            value = { this.state.btcNewBalance || this.state.btcBalance}
+            placeholder = {'0.00000001'}
+            type = "text"
+            readOnly
+          />
 
           <label> DOGE Price: </label>
           <input
@@ -98,10 +168,56 @@ class Form extends React.Component {
             onChange = {this.handleOnChange}
           />
 
+          <label> Fee: </label>
+          <input
+            name = {'fee'}
+            value = {this.state.fee}
+            placeholder = {'0.00000001'}
+            type = "text"
+            readOnly
+          />
+
+          <label> Total: </label>
+          <input
+            name = {'total'}
+            value = {this.state.total}
+            placeholder = {'0.00000001'}
+            type = "text"
+            readOnly
+          />
+
+          <label> BTC Price: </label>
+          <input
+            name = {'btcPrice'}
+            value = {this.state.btcPrice}
+            placeholder = {'$0.00'}
+            type = "text" 
+            onChange = {this.handleOnChange}
+          />
+
+          <label> USD Amount: </label>
+          <input
+            name = {'usdAmount'}
+            value = {this.state.usdAmount}
+            placeholder = {'$0.00'}
+            type = "text"
+            onChange = {this.handleOnChange}
+          />
+
+          <label> BTC Quantity: </label>
+          <input
+            name = {'btcQuantity'}
+            value = {this.state.btcQuantity}
+            placeholder = {'0.00000000'}
+            type = "text"
+            onChange = {this.handleOnChange}
+          />
+
           <button type = {'submit'}>submit</button>
 
         </form>
-      </div>
+
+      </FormContent>
     ) 
   }
 }
