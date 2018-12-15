68b818adea6679f981a16d71ef860910d75092a6
Dennis Orbison <orbison.dennis@gmail.com>
2018-12-15 14:52:53 -0800

    added dependencies router, react-router-dom

--- trade-crypto/src/index.js
+++ trade-crypto/src/index.js
@@ -2,5 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(() => null);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>
+, document.getElementById('root'));
